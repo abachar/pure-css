3a5ca6a02d1a827087efafd42929691be718d4cc
Abdelhakim Bachar <abachar@MBPdeAbdelhakim.bouyguesbox.fr>
2015-02-23 00:43:36 +0100

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var gulp    = require('gulp'),
     connect = require('gulp-connect'),
-    less    = require('gulp-less');
+    less    = require('gulp-less'),
+    inject  = require('gulp-inject');
  
 gulp.task('connect', function() {
   connect.server({
@@ -10,8 +13,19 @@ gulp.task('connect', function() {
   });
 });
 
+gulp.task('font', function() {
+  gulp.src('src/fonts/*')
+      .pipe(gulp.dest('dist/fonts'));
+});
+
 gulp.task('html', function () {
-  gulp.src('src/*.html')
+  gulp.src('src/index.html')
+      .pipe(inject(gulp.src(['src/partials/*.html']), {
+          starttag: '<!-- inject:html -->',
+          transform: function (filePath, file) {
+              return file.contents.toString('utf8')
+          }
+      }))
       .pipe(gulp.dest('dist'))
       .pipe(connect.reload());
 });
@@ -24,8 +38,8 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/styles/*.less', ['styles']);
-  gulp.watch('src/*.html', ['html']);
+  gulp.watch('src/styles/**/*.less', ['styles']);
+  gulp.watch('src/**/*.html', ['html']);
 });
 
-gulp.task('default', ['styles', 'html', 'connect', 'watch']);
+gulp.task('default', ['font', 'styles', 'html', 'connect', 'watch']);
